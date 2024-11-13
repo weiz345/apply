9651e9085db61331bc52787ffe858b9e2aa6d503
zhengw <zhengw@fortinet.com>
2024-11-13 00:41:57 -0800

    add print db

---
+++ print_db.py
@@ -0,0 +1,14 @@
+from app import app
+from extensions import db
+from models import User
+
+with app.app_context():
+    users = User.query.all()
+    for user in users:
+        print(f'ID: {user.id}')
+        print(f'Email: {user.email}')
+        print(f'Verified: {user.is_verified}')
+        print(f'Gmail Email: {user.gmail_email}')
+        print(f'Gmail App Password: {user.gmail_app_password}')
+        print(f'Resume: {user.resume}')
+        print('-' * 40)
