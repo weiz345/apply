8cf7403aea9392302a8ae4c64f2fe49023ab4e3f
zhengw <zhengw@fortinet.com>
2024-11-13 23:55:24 -0800

    posgres

--- config.py
+++ config.py
@@ -1,11 +1,18 @@
 import os
+from pathlib import Path
+
+basedir = Path(__file__).resolve().parent  # Sets the base directory for the app
+DATABASE = 'site.db'  # Define the database filename
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    url = os.getenv('DATABASE_URL', f'sqlite:///{Path(basedir).joinpath(DATABASE)}')
+    if url.startswith("postgres://"):
+        url = url.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'zhengweihang79@gmail.com'
-    MAIL_PASSWORD = 'pnps vkxl pgvi djfx'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'zhengweihang79@gmail.com'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'pnps vkxl pgvi djfx'
